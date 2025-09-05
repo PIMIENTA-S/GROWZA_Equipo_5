8ad6ae817fc455e560d7b2e94348fb27629d9fd3
caroo <dianacarolinacatama@gmail.com>
2025-09-05 15:52:02 -0500

    inicio de sesion

--- src/pages/registrarse/registrarse.js
+++ src/pages/registrarse/registrarse.js
@@ -5,57 +5,65 @@ const direccion = document.getElementById("inputdireccion");
 const email = document.getElementById("inputemail");
 const contraseña = document.getElementById("inputcontraseña");
 const btnRegistrar = document.getElementById("btn-registrar");
-const contendoralerta = document.getElementById("alerta-container")
+const contendoralerta = document.getElementById("alerta-container");
 
-function mostrarAlerta (mensaje, tipo = "danger"){
-    contendoralerta.innerHTML = `<div class="alert alert-${tipo} alert-dismissible fade show p-2" role="alert">
+function mostrarAlerta(mensaje, tipo = "danger") {
+    contendoralerta.innerHTML = `
+        <div class="alert alert-${tipo} alert-dismissible fade show p-2" role="alert">
             ${mensaje}
             <button type="button" class="btn-close p-3" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>`;
 }
 
-function validarEmail (email){
+function validarEmail(email) {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
 }
 
-function registrar(e){
+function registrar(e) {
     e.preventDefault();
 
-    if(!nombre.value.trim() || !apellido.value.trim() || !direccion.value.trim()){
-        mostrarAlerta("Todos los campos son obligatorios","danger");
-        return
-    }
-
-    if(contraseña.value.length < 8){
-        mostrarAlerta("La contraseña debe tener al menos 8 caracteres","warning");
+    if (!nombre.value.trim() || !apellido.value.trim() || !direccion.value.trim() || !email.value.trim() || !contraseña.value.trim()) {
+        mostrarAlerta("Todos los campos son obligatorios", "danger");
         return;
     }
 
-    const existente = usuarios.find(u => u.email.value);
-    if(existente){
-        mostrarAlerta("Ese correo ya se encuentra registrado","danger");
+    if (!validarEmail(email.value)) {
+        mostrarAlerta("Por favor ingrese un email válido", "warning");
         return;
     }
+
+    if (contraseña.value.length < 8) {
+        mostrarAlerta("La contraseña debe tener al menos 8 caracteres", "warning");
+        return;
+    }
+
+    const existente = usuarios.find(u => u.email === email.value);
+    if (existente) {
+        mostrarAlerta("Ese correo ya se encuentra registrado", "danger");
+        return;
+    }
+
 
     const usuario = {
-        Nombre: nombre.value,
-        Apellido: apellido.value,
-        Direccion: direccion.value,
-        Email: email.value,
-        Contraseña: contraseña.value
+        nombre: nombre.value,
+        apellido: apellido.value,
+        direccion: direccion.value,
+        email: email.value,
+        password: contraseña.value,
+        rol:"usuario"
     };
 
     usuarios.push(usuario);
     localStorage.setItem("usuarios", JSON.stringify(usuarios));
-    mostrarAlerta("Usuario registrado correctamente","success");
+    mostrarAlerta("Usuario registrado correctamente", "success");
+
 
     nombre.value = "";
     apellido.value = "";
     direccion.value = "";
     email.value = "";
     contraseña.value = "";
-
-};
+}
 
 btnRegistrar.addEventListener("click", registrar);
